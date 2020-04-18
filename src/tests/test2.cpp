ad49b74d32e79dfb28dd9d668580d9cdcc0246e9
2iw31Zhv <ziweizhu95@gmail.com>
2020-04-17 22:39:32 -0400

    _

--- src/tests/test2.cpp
+++ src/tests/test2.cpp
@@ -7,9 +7,17 @@ using namespace std;
 int main()
 {
 	DataBase<string, string> db;
-
 	cout << db.set("hello", "world", 'n') << '\n';
 	cout << db.set("hello", "new", 'n') << '\n';
 	cout << db.get("hello") << '\n';
+
+	// this does not compile
+	// DataBase<int, int> db;
+	// cout << db.set(1, 2) << '\n';
+	// cout << db.get(1) << '\n';
+
+	// we need to specialize a class each key type
+	// DataBase<SupportedKey, string> db;
+
 	return 0;
 }
