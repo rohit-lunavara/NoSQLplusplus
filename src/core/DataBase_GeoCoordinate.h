cc7ff04da3b837825f8e6215ebe2a6f539e378a1
2iw31Zhv <ziweizhu95@gmail.com>
2020-04-29 00:02:02 -0400

    add some opts

--- src/core/DataBase_GeoCoordinate.h
+++ src/core/DataBase_GeoCoordinate.h
@@ -1,6 +1,8 @@
 #include "HashTable.h"
 #include "GeoCoordinate.h"
 #include "SphericalRTree.h"
+#include "DataBase_Errors.h"
+
 #include <set>
 
 template <>
@@ -15,6 +17,7 @@ public:
         bool set(const std::string& key, const Geography::GeoCoordinate& value,
                 std::initializer_list<std::string> options);
         
+        bool exist(const std::string& key);
         bool remove(const std::string& key);
         bool rename(const std::string& key, const std::string& newkey);
 
@@ -33,6 +36,7 @@ bool DataBase<std::string, Geography::GeoCoordinate>::set(
         const Geography::GeoCoordinate& value, 
         std::initializer_list<std::string> options)
 {
+        // TODO options
         rtree_.insert(key, value, 0.);
         table_[key] = value;
         return true;
@@ -42,25 +46,62 @@ bool DataBase<std::string, Geography::GeoCoordinate>::set(
 Geography::GeoCoordinate
 DataBase<std::string, Geography::GeoCoordinate>::get(const std::string& key)
 {
+	if (!exist(key))
+        {
+		throw ValueNotFoundException();
+	}
         return table_[key];
 }
 
 // O (N)
 bool DataBase<std::string, Geography::GeoCoordinate>::remove(const std::string& key)
 {
+        if (!exist(key))
+        {
+                return false;
+        }
+
         rtree_.remove(key, table_[key], 0.001);
         table_.erase(key);
         return true;
 }
+
+
+bool DataBase<std::string, Geography::GeoCoordinate>::exist(const std::string& key)
+{
+        return table_.find(key) != table_.end();
+}
+
+
+Geography::GeoCoordinate DataBase<std::string, Geography::GeoCoordinate>::getset(
+        const std::string& key,
+        const Geography::GeoCoordinate& value,
+        std::initializer_list<std::string> options)
+{
+        if (!exist(key))
+        {
+                throw ValueNotFoundException();
+        }
+
+        auto oldvalue = get(key);
+        set(key, value, options);
+        return oldvalue;
+}
+
 
 // O (1)
 double DataBase<std::string, Geography::GeoCoordinate>::distance(
     const std::string& key1, 
     const std::string& key2)
 {
-    const auto& geo1 = table_[key1];
-    const auto& geo2 = table_[key2];
-    return Geography::distance(geo1, geo2);
+        if (!exist(key1) || !exist(key2))
+        {
+                throw ValueNotFoundException();
+        }
+
+        const auto& geo1 = table_[key1];
+        const auto& geo2 = table_[key2];
+        return Geography::distance(geo1, geo2);
 }
 
 
@@ -69,7 +110,29 @@ std::set<std::string> DataBase<std::string, Geography::GeoCoordinate>::radius(
         const std::string& key, 
         double rad)
 {
+        if (!exist(key))
+        {
+                throw ValueNotFoundException();
+        }
+
         std::set<std::string> results;
         rtree_.search(table_[key], rad, &results);
         return results;
+}
+
+
+bool DataBase<std::string, Geography::GeoCoordinate>::rename(
+        const std::string& key, 
+        const std::string& newkey)
+{
+        if (!exist(key))
+        {
+                return false;
+        }
+
+        auto value = get(key);
+        remove(key);
+        set(newkey, value, {});
+
+        return true;
 }
