6c5fff4b03c09f92aafff1878611ae57306761b5
2iw31Zhv <ziweizhu95@gmail.com>
2020-04-30 19:30:53 -0400

    add comments

--- src/core/DataBase_GeoCoordinate.h
+++ src/core/DataBase_GeoCoordinate.h
@@ -5,28 +5,89 @@
 
 #include <set>
 #include <locale>
-
+/**
+ * Specialized class for Geography Coodinates (latitude and longitude) handling
+ */
 template <>
 class DataBase<std::string, Geography::GeoCoordinate>
 {
 private:
+        /**
+         * data structure for spherical rtree
+         */
         RTree::SphericalRTree<std::string> rtree_;
+
+        /**
+         * a hashtable keep the record of geography coordinates
+         * for each key
+         */
         HashTable<std::string, Geography::GeoCoordinate> table_;
 public:
+        /**
+         * Gets the geography coordinates for a key
+         * @param key key for coordinates, such as city name
+         * @return geography coordinates, if not exist throw an error
+         */
         Geography::GeoCoordinate get(const std::string& key);
 
+        /**
+         * Sets the coordinate for a key
+         * with options "NX", only set if the key does not exist
+         *              "XX", only set if the key exists
+         * @param key key for coordinates
+         * @param value coordinates
+         * @param options different options {opt1, opt2}
+         * @return true if successful
+         */
         bool set(const std::string& key, const Geography::GeoCoordinate& value,
                 std::initializer_list<std::string> options = {});
         
+        /**
+         * Checks if a key exist
+         * @param key key for coordinates
+         * @return true if existed
+         */
         bool exist(const std::string& key);
+
+        /**
+         * Removes a key and the corresponding record
+         * @return true if the key exist
+         */
         bool remove(const std::string& key);
+
+        /**
+         * Renames a key
+         @param key key for a coordinate
+         @param newKey the new key for this coordinate
+         @return true if the old key exist
+         */
         bool rename(const std::string& key, const std::string& newkey);
 
+        /**
+         * Gets the coordinates for a key and set them to be value
+         * with options
+         * @param key key for the coordinate
+         * @param value the new value for the key
+         * @param options options
+         * @return old geography coordinates
+         */
         Geography::GeoCoordinate getset(const std::string& key, const Geography::GeoCoordinate& value,
                 std::initializer_list<std::string> options);
         
+        /**
+         * Gets the distance between two keys: key1 and key2
+         * @param key1 one key
+         * @param key2 another key
+         * @return true if both key exist or throw an error
+         */
         double distance(const std::string& key1, const std::string& key2);
         
+        /** 
+         * Queries the keys within distance rad from key
+         * @param key the center key
+         * @param rad the query radiance, in kilometers
+         * @return the set for all keys located in the range
+         */
         std::set<std::string> radius(const std::string& key, double rad);
 };
 
